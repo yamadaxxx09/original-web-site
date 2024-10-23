f218f2003a23b08ebe0a9a8055df7320f09f870c
yamadaxxx09 <152535603+yamadaxxx09@users.noreply.github.com>
2024-10-22 19:36:43 -0700

    改善

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,29 @@
 
+// $(function() {
+//   $(".question").click(function() {
+//     $(this).next(".answer").slideToggle(); 
+
+//       if(event.type === "touchstart") {
+//         event.preventDefault();
+//       }
+
+//       $(this).next(".answer").slideToggle();
+//   });
+
+// });
+
 $(function() {
-  $(".question").click(function() {
-    $(this).next(".answer").slideToggle(); 
+  let isTouchDevice = false;
 
-      if(event.type === "touchstart") {
-        event.preventDefault();
-      }
-
-      $(this).next(".answer").slideToggle();
+  $(".question").on("touchstart", function(event) {
+    isTouchDevice = true;
+    $(this).next(".answer").slideToggle();
+    event.preventDefault(); 
   });
 
+  $(".question").on("click", function(event) {
+    if (!isTouchDevice) {
+      $(this).next(".answer").slideToggle();
+    }
+  });
 });
